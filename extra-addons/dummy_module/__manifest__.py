ecb4660749c5822efbe8955946ab1abbd827f54b
Risco Velasco <alejandro.risco@isepceu.es>
2025-12-12 20:00:59 +0100

    Corrección crítica: Limpiar sintaxis de manifiesto

--- extra-addons/dummy_module/__manifest__.py
+++ extra-addons/dummy_module/__manifest__.py
@@ -1,13 +1,13 @@
 {
-    [cite_start]"name": "Biblioteca Alumno", [cite: 47]
-    [cite_start]"version": "1.0", [cite: 48]
-    [cite_start]"summary": "Módulo de práctica: gestión de libros", [cite: 49]
-    [cite_start]"author": "Alumno DAM", [cite: 50]
-    [cite_start]"category": "Teaching", [cite: 51]
-    [cite_start]"depends": ["base"], [cite: 52]
+    "name": "Biblioteca Alumno",
+    "version": "1.0",
+    "summary": "Módulo de práctica: gestión de libros",
+    "author": "Alumno DAM",
+    "category": "Teaching",
+    "depends": ["base"],
     "data": [
-        [cite_start]"views/biblioteca_libro_views.xml", [cite: 56]
+        "views/biblioteca_libro_views.xml",
     ],
-    [cite_start]"installable": True, [cite: 58]
-    [cite_start]"application": True, [cite: 59]
+    "installable": True,
+    "application": True,
 }
