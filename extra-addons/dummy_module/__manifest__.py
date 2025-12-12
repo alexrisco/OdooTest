1bd07f78962e269ce25e131dcfaae6b812302b89
Risco Velasco <alejandro.risco@isepceu.es>
2025-12-12 18:04:53 +0100

    Implementación del módulo Biblioteca Alumno y corrección del manifiesto dummy

--- extra-addons/dummy_module/__manifest__.py
+++ extra-addons/dummy_module/__manifest__.py
@@ -1,6 +1,13 @@
 {
-    "name": "Dummy Module",
-    "version": "1.0",
-    "summary": "Módulo vacío de prueba",
-    "installable": True,
+    [cite_start]"name": "Biblioteca Alumno", [cite: 47]
+    [cite_start]"version": "1.0", [cite: 48]
+    [cite_start]"summary": "Módulo de práctica: gestión de libros", [cite: 49]
+    [cite_start]"author": "Alumno DAM", [cite: 50]
+    [cite_start]"category": "Teaching", [cite: 51]
+    [cite_start]"depends": ["base"], [cite: 52]
+    "data": [
+        [cite_start]"views/biblioteca_libro_views.xml", [cite: 56]
+    ],
+    [cite_start]"installable": True, [cite: 58]
+    [cite_start]"application": True, [cite: 59]
 }
